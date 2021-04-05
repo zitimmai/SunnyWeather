45017f46952e3ee3943e25fce46b7f9e20d4d0a7
zitimmai <842831540@qq.com>
2021-04-05 13:20:57 +0800

    2021-04-05 Modify.

---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -0,0 +1,63 @@
+package com.sunnyweather.android.ui.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sunnyweather.android.R
+import kotlinx.android.synthetic.main.fragment_place.*
+
+class PlaceFragment : Fragment(){
+
+    val viewModel by lazy {
+        ViewModelProvider(this).get(PlaceViewModel::class.java)
+    }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.adapter = adapter
+        //搜索城市数据请求的发起
+        //searchPlaceEdit组件动态监控搜索框内容的变化，如果有内容则发起searchPlaces方法，没有内容则隐藏recyclerView
+        searchPlaceEdit.addTextChangedListener{ editable->
+            val content = editable.toString()
+            if (content.isNotEmpty()){
+                viewModel.searchPlaces(content)
+            }else{
+                recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        //接收相应的数据
+        viewModel.placeLiveData.observe(this, Observer { result->
+            val places = result.getOrNull()
+            if (places != null){
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            }else{
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+
+}
